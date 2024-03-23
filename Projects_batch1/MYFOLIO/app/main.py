f2e715ed3a1e985ad106137759a9e32e8142180e
shrilc <shrilc@Mark-I.local>
2024-03-23 22:23:26 +0530

    second commit

--- Projects_batch1/MYFOLIO/app/main.py
+++ Projects_batch1/MYFOLIO/app/main.py
@@ -18,7 +18,39 @@ def download_resume():
 
 @app.route('/projects')
 def projects():
-    return render_template("projects.html")
+    cards = [
+        {
+            'title': 'Project1',
+            'image': 'https://images.pexels.com/photos/3183150/pexels-photo-3183150.jpeg?auto=compress&cs=tinysrgb&w=500&h=300&dpr=2',
+            'description': 'Some desc'
+        },
+        {
+            'title': 'Project2',
+            'image': 'https://images.pexels.com/photos/3183150/pexels-photo-3183150.jpeg?auto=compress&cs=tinysrgb&w=500&h=300&dpr=2',
+            'description': 'Some desc'
+        },
+        {
+            'title': 'Project3',
+            'image': 'https://images.pexels.com/photos/3183150/pexels-photo-3183150.jpeg?auto=compress&cs=tinysrgb&w=500&h=300&dpr=2',
+            'description': 'Some desc'
+        },
+        {
+            'title': 'Project3',
+            'image': 'https://images.pexels.com/photos/3183150/pexels-photo-3183150.jpeg?auto=compress&cs=tinysrgb&w=500&h=300&dpr=2',
+            'description': 'Some desc'
+        },
+        {
+            'title': 'Project3',
+            'image': 'https://images.pexels.com/photos/3183150/pexels-photo-3183150.jpeg?auto=compress&cs=tinysrgb&w=500&h=300&dpr=2',
+            'description': 'Some desc'
+        },
+        {
+            'title': 'Project3',
+            'image': 'https://images.pexels.com/photos/3183150/pexels-photo-3183150.jpeg?auto=compress&cs=tinysrgb&w=500&h=300&dpr=2',
+            'description': 'Some desc'
+        }
+    ]
+    return render_template("projects.html", cards=cards)
 
 
 if __name__ == "__main__":
